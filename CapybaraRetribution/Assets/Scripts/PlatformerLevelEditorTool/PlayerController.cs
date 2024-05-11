1067b6e415ea887a17f14a407b80433da6d33310
Drago1426 <peter.sheehen.c10396@mcast.edu.mt>
2024-05-11 14:19:36 +0200

    Platformer Level Editor Tool
    Created player controller for health and movement. Enemy Controller also created to detect when player collides with enemy.

--- CapybaraRetribution/Assets/Scripts/PlatformerLevelEditorTool/PlayerController.cs
+++ CapybaraRetribution/Assets/Scripts/PlatformerLevelEditorTool/PlayerController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float jumpForce = 7.0f;
+    [SerializeField] private int maxHealth = 100;
+
+    private Rigidbody2D rb;
+    private int currentHealth;
+    private bool isGrounded;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
         
+        if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Player Health: " + currentHealth);
+        
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Player is dead!");
+        }
     }
 }
