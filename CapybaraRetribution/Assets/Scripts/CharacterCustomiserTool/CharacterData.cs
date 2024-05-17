687f28c326be5211e326c173b22e7131e9607f9a
Louisann-Borg <122116816+Louisann-Borg@users.noreply.github.com>
2024-05-17 19:46:01 +0200

    Portals, Character Creator Update
    Made the portals work and adjusted the character creator so it loads selected player assets

--- CapybaraRetribution/Assets/Scripts/CharacterCustomiserTool/CharacterData.cs
+++ CapybaraRetribution/Assets/Scripts/CharacterCustomiserTool/CharacterData.cs
@@ -1,10 +1,8 @@
-﻿using UnityEngine;
-
-    [System.Serializable]
-    public class CharacterData
-    {
-        public int furIndex;
-        public int eyesIndex;
-        public int hatIndex;
-        public int weaponIndex;
-    }
+﻿[System.Serializable]
+public class CharacterData
+{
+    public int furIndex;
+    public int eyesIndex;
+    public int hatIndex;
+    public int weaponIndex;
+}
